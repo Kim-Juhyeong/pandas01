f713675d6b2d9c1e6075d986af405f013ea42eca
Kim Juhyeong <juhyeong_kim@naver.com>
2024-12-09 02:09:24 +0900

    commit_visualization

--- pandas.py
+++ pandas.py
@@ -1,15 +1,24 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('./driver_license_status1.csv')
 
-print(df)
+grouped_df = df.groupby(['year', 'age2'])[['Class1 Heavy','Class1 Ordinary','Class2 Ordinary','Class2 small']].sum().reset_index()
 
-grouped = df.groupby(['year','age2'])
-average = grouped.mean()
+grouped
 
-for key, group in grouped:
-    print(group.head())
-    print(group.tail())
-    print('\n')
+age2_20 = grouped_df[grouped_data['age2'] == 20]
+age2_30 = grouped_df[grouped_data['age2'] == 30]
 
-average
+plt.figure(figsize=(12, 6))
+
+for column in ['Class1 Heavy','Class1 Ordinary','Class2 Ordinary','Class2 small']:
+    plt.plot(age2_20['year'], age2_20[column], label=f'age2=20, {column}')
+    plt.plot(age2_30['year'], age2_30[column], linestyle='--', label=f'age2=30, {column}')
+
+plt.title('Changes in License Holders by Year and Age Group (age2=20 & age2=30)')
+plt.xlabel('Year')
+plt.ylabel('Number of License Holders')
+plt.legend()
+plt.grid(True)
+plt.show()
